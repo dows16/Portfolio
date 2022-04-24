a715a8d9cffcff12f116c372c10c2164d547e976
Dowson <dcheung@smail.pembinatrails.ca>
2022-04-24 18:46:05 -0500

    first commit

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -13,34 +13,46 @@ const Experience = () => {
           <h3>3D Modeling (Blender)</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <RiCheckboxCircleFill />
-              <h4>Vehicles</h4>
-              <small className="text-light">Experienced</small>
+              <RiCheckboxCircleFill className="experience__details-icons"/>
+              <div>
+                <h4>Vehicles</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
-              <RiCheckboxCircleFill />
-              <h4>Aeroplanes</h4>
-              <small className="text-light">Intermediate</small>
+              <RiCheckboxCircleFill className="experience__details-icons"/>
+              <div>
+                <h4>Aeroplanes</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <RiCheckboxCircleFill />
-              <h4>Architecture</h4>
-              <small className="text-light">Experienced</small>
+              <RiCheckboxCircleFill className="experience__details-icons"/>
+              <div>          
+                <h4>Architecture</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
-              <RiCheckboxCircleFill />
-              <h4>Boats</h4>
-              <small className="text-light">Intermediate</small>
+              <RiCheckboxCircleFill className="experience__details-icons"/>
+              <div>
+                <h4>Boats</h4>
+                <small className="text-light">Intermediate</small>
+              </div>  
             </article>
             <article className="experience__details">
-              <RiCheckboxCircleFill />
-              <h4>Assets</h4>
-              <small className="text-light">Intermediate</small>
+              <RiCheckboxCircleFill className="experience__details-icons"/>
+              <div>
+                <h4>Assets</h4>
+                <small className="text-light">Intermediate</small>
+              </div>  
             </article>
             <article className="experience__details">
-              <RiCheckboxCircleFill />
-              <h4>Accessories</h4>
-              <small className="text-light">Beginner</small>
+              <RiCheckboxCircleFill className="experience__details-icons"/>
+              <div>
+                <h4>Accessories</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
           </div>
         </div>
@@ -49,29 +61,39 @@ const Experience = () => {
           <h3>Frontend Web Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <RiCheckboxCircleFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <RiCheckboxCircleFill className="experience__details-icons"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
-              <RiCheckboxCircleFill />
-              <h4>CSS</h4>
-              <small className="text-light">Intermediate</small>
+              <RiCheckboxCircleFill className="experience__details-icons"/>
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <RiCheckboxCircleFill />
-              <h4>Javascript</h4>
-              <small className="text-light">Beginner</small>
+              <RiCheckboxCircleFill className="experience__details-icons"/>
+              <div>
+                <h4>Javascript</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
             <article className="experience__details">
-              <RiCheckboxCircleFill />
-              <h4>ReactJS</h4>
-              <small className="text-light">Intermediate</small>
+              <RiCheckboxCircleFill className="experience__details-icons"/>
+              <div>
+                <h4>ReactJS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <RiCheckboxCircleFill />
-              <h4>ReactJSX</h4>
-              <small className="text-light">Intermediate</small>
+              <RiCheckboxCircleFill className="experience__details-icons"/>
+              <div>
+                <h4>ReactJSX</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
           </div>
         </div>
