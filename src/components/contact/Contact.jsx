0479acddfe6b7744343ff258df6e58a81e7a48d5
Dowson <dcheung@smail.pembinatrails.ca>
2022-05-03 18:26:26 -0500

    first commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,28 +12,37 @@ const Contact = () => {
 
       <div className="container contact__container">
         <div className="contact__options">
+          {/* The link opens up your mail app/a new email tab (gmail on your browser) and creates a draft email having it set so the receiving email is 'dowson2022@gmail.com' */}
           <article className="contact__option">
             <MdMailOutline />
             <h4>Email</h4>
             <h5>dowson2022@gmail.com</h5>
-            <a href="mailto:dowson2022@gmail.com">Send an email</a>
+            <a href="mailto:dowson2022@gmail.com" target="_blank">Send an email</a>
           </article>
+          {/* The link brings you to the discord website */}
           <article className="contact__option">
             <FaDiscord />
             <h4>Discord</h4>
             <h5>dows#7844</h5>
-            <a href="discord.com/app">Send a friend request</a>
+            <a href="https://www.discord.com/app" target="_blank">Send a friend request</a>
           </article>
-          {/* The link to messenger doesn't work since I don't have messenger so the one below is an example*/}
+          {/* The link to messenger doesn't work since I don't have messenger but it does bring you to the messenger website*/}
           <article className="contact__option">
             <RiMessengerLine />
             <h4>Messenger</h4>
             <h5>dowsoncheung26</h5>
-            <a href="https://m.me/dowsoncheung26">Send a message</a>
+            <a href="https://m.me/dowsoncheung26" target="_blank">Send a message</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action=""></form>
+        {/* Below I madea a form option creating a direct email message, I also made it so input is required and there won't be any empty inputs*/}
+        <form action="">
+          <input type="text" name="name" placeholder="Enter your full name" required />
+          <input type="email" name="email" placeholder="Enter your email address" required />
+          {/* textarea is a frequently used form control, which is used to get multi-line input from a user. It's different from a normal text input, which allows only single-line input.*/}
+          <textarea name="message" rows="7" placeholder="Enter your message" required />
+          <button type="submit" className="btn">Submit Message</button>
+        </form>
       </div>
     </section>
   )
