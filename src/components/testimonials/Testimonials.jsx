2b2faebd02648bdc4bc39e6cc043778c4b55d25f
Dowson <dcheung@smail.pembinatrails.ca>
2022-05-02 19:54:21 -0500

    first commit

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,15 +4,25 @@ import AVTR1 from "../../assets/avatar1.jpg"
 import AVTR2 from "../../assets/avatar2.jpg"
 import AVTR3 from "../../assets/avatar3.jpg"
 import AVTR4 from "../../assets/avatar4.jpg"
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Testimonials = () => {
   return (
     <section id="testimonials">
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
-
-      <div className="container testimonials__container">
-        <article className="testimonial">
+      
+      <Swiper className="container testimonials__container">
+        <SwiperSlide className="testimonial">
           <div className="client__avatar">
             <img src={AVTR1} alt="Photo of First Client" />
           </div>
@@ -20,8 +30,8 @@ const Testimonials = () => {
           <small className="client__review">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempora repellat inventore, natus amet et obcaecati molestias nisi ea unde doloremque, possimus vel! Sint explicabo, non aspernatur aliquid veniam dolore.
             </small>
-        </article>
-        <article className="testimonial">
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
           <div className="client__avatar">
             <img src={AVTR2} alt="Photo of Second Client" />
           </div>
@@ -29,8 +39,8 @@ const Testimonials = () => {
           <small className="client__review">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempora repellat inventore, natus amet et obcaecati molestias nisi ea unde doloremque, possimus vel! Sint explicabo, non aspernatur aliquid veniam dolore.
             </small>
-        </article>
-        <article className="testimonial">
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
           <div className="client__avatar">
             <img src={AVTR3} alt="Photo of Third Client" />
           </div>
@@ -38,8 +48,8 @@ const Testimonials = () => {
           <small className="client__review">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempora repellat inventore, natus amet et obcaecati molestias nisi ea unde doloremque, possimus vel! Sint explicabo, non aspernatur aliquid veniam dolore.
             </small>
-        </article>
-        <article className="testimonial">
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
           <div className="client__avatar">
             <img src={AVTR4} alt="Photo of Forth Client" />
           </div>
@@ -47,8 +57,8 @@ const Testimonials = () => {
           <small className="client__review">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempora repellat inventore, natus amet et obcaecati molestias nisi ea unde doloremque, possimus vel! Sint explicabo, non aspernatur aliquid veniam dolore.
             </small>
-        </article>
-      </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   )
 }
